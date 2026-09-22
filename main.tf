dbfe4cae4c86c03831d13215773da5d8bac47503
Ritesh Jagtap <ritesh.s.jagtap@accenture.com>
2026-09-22 15:38:30 +0530

    S3 Bucket for Production

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "dev_bucket" {
+resource "aws_s3_bucket" "prod_bucket" {
   bucket = var.bucket_name
 
   tags = {
-    Environment = "int"
+    Environment = "Prod"
     ManagedBy   = "spacelift"
   }
 }
 
-resource "aws_s3_bucket_versioning" "dev_bucket" {
+resource "aws_s3_bucket_versioning" "prod_bucket" {
   bucket = aws_s3_bucket.dev_bucket.id
   versioning_configuration {
     status = "Enabled"
--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "bucket_name" {
-  value = aws_s3_bucket.dev_bucket.bucket
+  value = aws_s3_bucket.prod_bucket.bucket
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.dev_bucket.arn
+  value = aws_s3_bucket.prod_bucket.arn
 }
